5191e9aeb6fe9d8667f4e4755569baf44d20adc1
"Luke Fraser" <lukefrasera@gmail.com>
2015-04-19 13:07:27 -0700

    the skeleton now works with the new setup. There is still an issue with the conversion from float which is ros based into the usigned 16bit image.

--- src/seq_skel_track.cc
+++ src/seq_skel_track.cc
@@ -16,8 +16,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "seq_skel_track.h"
+#include <glib-object.h>
 
 namespace sk_track {
+
+static gfloat SMOOTHING_FACTOR = 0.2;
+
 
 #define FAR_CUTOFF 2.0
 #define CLOSE_CUTOFF 0.9
@@ -28,7 +32,8 @@ struct BufferInfo {
 };
 SeqSkelTrack::SeqSkelTrack() {
   skeleton = skeltrack_skeleton_new();
-  vis_pub = nh_.advertise<visualization_msgs::MarkerArray>("skeleton_markers", 0);
+  vis_pub = nh_.advertise<visualization_msgs::MarkerArray>("skeleton_markers",
+    0);
 }
 SeqSkelTrack::~SeqSkelTrack() {}
 
@@ -41,7 +46,8 @@ static void OnTrack(GObject *obj, GAsyncResult *res, gpointer user_data) {
   g_slice_free(BufferInfo, user_data);
 }
 
-visualization_msgs::Marker JointToMarker(SkeltrackJoint *joint,unsigned int id) {
+visualization_msgs::Marker JointToMarker(SkeltrackJoint *joint,
+    unsigned int id) {
   visualization_msgs::Marker marker;
 
   marker.header.frame_id = "camera_link";
@@ -50,9 +56,9 @@ visualization_msgs::Marker JointToMarker(SkeltrackJoint *joint,unsigned int id) 
   marker.id = id;
   marker.type = visualization_msgs::Marker::SPHERE;
   marker.action = visualization_msgs::Marker::ADD;
-  marker.pose.position.x = joint->x/100.0;
-  marker.pose.position.y = joint->y/100.0;
-  marker.pose.position.z = joint->z/100.0;
+  marker.pose.position.x = joint->screen_x/100.0;
+  marker.pose.position.y = joint->screen_y/100.0;
+  marker.pose.position.z = 0;
   marker.pose.orientation.x = 0.0;
   marker.pose.orientation.y = 0.0;
   marker.pose.orientation.z = 0.0;
@@ -67,25 +73,28 @@ visualization_msgs::Marker JointToMarker(SkeltrackJoint *joint,unsigned int id) 
 
   return marker;
 }
-void SeqSkelTrack::TrackSkel(cv_bridge::CvImageConstPtr data, guint width, guint height) {
-  cv::Mat thresh_image;
-  // float min, max;
-  // float *img_ptr = reinterpret_cast<float*>(data->image.data);
-  // min = max = img_ptr[0];
-  // for (int i = 0; i < width * height; ++i) {
-  //   if (min > img_ptr[i]) {min = img_ptr[i];}
-  //   if (max < img_ptr[i]) {max = img_ptr[i];}
-  // }
-  // printf("Min: %f, Max:%f\n", min, max);
-  cv::threshold(data->image, thresh_image, CLOSE_CUTOFF, 0, CV_THRESH_TOZERO);
-  cv::threshold(thresh_image, thresh_image, FAR_CUTOFF, 0, CV_THRESH_TOZERO_INV);
+void SeqSkelTrack::TrackSkel(cv_bridge::CvImageConstPtr data, guint width,
+    guint height) {
 
-  cv::Mat *image = new cv::Mat(cv::Size(40,30), data->image.type());
-  cv::imshow(WINDOW, thresh_image);
+  clock_t c_start, c_end;
+  cv::Mat thresh_image;
+  cv::Mat convert_image(data->image.size(), CV_16U);
+  cv::threshold(data->image, thresh_image, CLOSE_CUTOFF, 0, CV_THRESH_TOZERO);
+  cv::threshold(thresh_image, thresh_image, FAR_CUTOFF, 0,
+    CV_THRESH_TOZERO_INV);
+
+  cv::Mat *image = new cv::Mat(cv::Size(80, 60), CV_16U);
+
+  float *img_ptr = reinterpret_cast<float*>(thresh_image.data);
+  uint16_t *conv_ptr = reinterpret_cast<uint16_t*>(convert_image.data);
+
+  for (int i = 0; i < width * height; ++i) {
+    conv_ptr[i] = img_ptr[i]*600;
+  }
+  // cv::Mat *image = new cv::Mat(data->image.size(), data->image.type());
+  cv::resize(convert_image, *image, image->size());
+  cv::imshow(WINDOW, convert_image*50);
   cv::waitKey(30);
-  cv::resize(thresh_image, *image, image->size());
-  *image = *image * 500;
-  image->convertTo(*image, CV_16U);
   // uint16_t *img_ptr = reinterpret_cast<uint16_t*>(image->data);
   // uint16_t min, max;
   // min = max = img_ptr[0];
@@ -110,6 +119,7 @@ void SeqSkelTrack::TrackSkel(cv_bridge::CvImageConstPtr data, guint width, guint
   //   NULL,
   //   OnTrack,
   //   userinfo);
+  c_start = clock();
   SkeltrackJointList list = skeltrack_skeleton_track_joints_sync(
     skeleton,
     ptr,
@@ -117,56 +127,59 @@ void SeqSkelTrack::TrackSkel(cv_bridge::CvImageConstPtr data, guint width, guint
     image->size().height,
     NULL,
     &error);
-  if(list != NULL) {
-    printf("Found Skeleton\n");
+
+  c_end = clock();
+  if (list != NULL) {
+    printf("%f\n", 1000.0 * (c_end-c_start) / CLOCKS_PER_SEC);
+    // printf("Found Skeleton\n");
     SkeltrackJoint *head = skeltrack_joint_list_get_joint(list,
-                                           SKELTRACK_JOINT_ID_HEAD);
+      SKELTRACK_JOINT_ID_HEAD);
     SkeltrackJoint *left_hand = skeltrack_joint_list_get_joint(list,
-                                                SKELTRACK_JOINT_ID_LEFT_HAND);
+      SKELTRACK_JOINT_ID_LEFT_HAND);
     SkeltrackJoint *right_hand = skeltrack_joint_list_get_joint(list,
-                                                 SKELTRACK_JOINT_ID_RIGHT_HAND);
+      SKELTRACK_JOINT_ID_RIGHT_HAND);
     SkeltrackJoint *left_shoulder = skeltrack_joint_list_get_joint(list,
-                                         SKELTRACK_JOINT_ID_LEFT_SHOULDER);
+      SKELTRACK_JOINT_ID_LEFT_SHOULDER);
     SkeltrackJoint *right_shoulder = skeltrack_joint_list_get_joint(list,
-                                         SKELTRACK_JOINT_ID_RIGHT_SHOULDER);
+      SKELTRACK_JOINT_ID_RIGHT_SHOULDER);
     SkeltrackJoint *left_elbow = skeltrack_joint_list_get_joint(list,
-                                                 SKELTRACK_JOINT_ID_LEFT_ELBOW);
+      SKELTRACK_JOINT_ID_LEFT_ELBOW);
     SkeltrackJoint *right_elbow = skeltrack_joint_list_get_joint(list,
-                                                  SKELTRACK_JOINT_ID_RIGHT_ELBOW);
+      SKELTRACK_JOINT_ID_RIGHT_ELBOW);
     visualization_msgs::MarkerArray marker_list;
 
     if (head) {
-      printf("Head\n");
+      // printf("Head\n");
       visualization_msgs::Marker marker = JointToMarker(head, 0);
       marker_list.markers.push_back(marker);
     }
     if (left_hand) {
-      printf("Left Hand\n");
+      // printf("Left Hand\n");
       visualization_msgs::Marker marker = JointToMarker(left_hand, 1);
       marker_list.markers.push_back(marker);
     }
     if (right_hand) {
-      printf("right_hand\n");
+      // printf("right_hand\n");
       visualization_msgs::Marker marker = JointToMarker(right_hand, 2);
       marker_list.markers.push_back(marker);
     }
     if (left_shoulder) {
-      printf("left_shoulder\n");
+      // printf("left_shoulder\n");
       visualization_msgs::Marker marker = JointToMarker(left_shoulder, 3);
       marker_list.markers.push_back(marker);
     }
     if (right_shoulder) {
-      printf("right_shoulder\n");
+      // printf("right_shoulder\n");
       visualization_msgs::Marker marker = JointToMarker(right_shoulder, 4);
       marker_list.markers.push_back(marker);
     }
     if (left_elbow) {
-      printf("left_elbow\n");
+      // printf("left_elbow\n");
      visualization_msgs::Marker marker  = JointToMarker(left_elbow, 5);
       marker_list.markers.push_back(marker);
     }
     if (right_elbow) {
-      printf("right_elbow\n");
+      // printf("right_elbow\n");
       visualization_msgs::Marker marker = JointToMarker(right_elbow, 6);
       marker_list.markers.push_back(marker);
     }
@@ -174,8 +187,9 @@ void SeqSkelTrack::TrackSkel(cv_bridge::CvImageConstPtr data, guint width, guint
     vis_pub.publish(marker_list);
 
     skeltrack_joint_list_free(list);
-  } else {printf("Not Found\n");}
+  }
   delete userinfo;
+  delete image;
 }
 
 void SeqSkelTrack::GetJoints() {
